930cb9407f358556e992e067add6de4ddd86884c
Zach Ahn <engineering@zachahn.com>
2024-01-11 09:26:40 -0700

    Implement "Value" operations

--- lib/micrbograd.rb
+++ lib/micrbograd.rb
@@ -1,6 +1,49 @@
 # frozen_string_literal: true
 
 module Micrbograd
-  class Error < StandardError; end
-  # Your code goes here...
+  class Value
+    def initialize(data, children: [], operation: nil)
+      @data = data
+      @children = children
+      @operation = operation
+      @grad = 0
+    end
+
+    attr_reader :data, :children, :operation, :grad
+    private :children, :operation
+
+    def +(other)
+      Value.new(data + other.data, children: [self, other], operation: :+)
+    end
+
+    def *(other)
+      Value.new(data * other.data, children: [self, other], operation: :*)
+    end
+
+    def **(other)
+      raise TypeError, "Value `#{data}' Only accepts type `Numeric'. Got #{other}" unless other.is_a?(Numeric)
+      Value.new(data**other, children: [self], operation: :**)
+    end
+
+    def tanh
+      Value.new(Math.tanh(data), children: [self], operation: :tanh)
+    end
+
+    def -@
+      other = Value.new(-1)
+      self * other
+    end
+
+    def -(other)
+      self + -other
+    end
+
+    def /(other)
+      self * (other**-1)
+    end
+
+    def to_s
+      "#<Micrbograd::Value @data=#{data}>"
+    end
+  end
 end
--- test/test_micrbograd.rb
+++ test/test_micrbograd.rb
@@ -3,4 +3,15 @@
 require "test_helper"
 
 class TestMicrbograd < Minitest::Test
+  include Micrbograd
+
+  def test_methods
+    assert_equal(7, (Value.new(3) + Value.new(4)).data)
+    assert_equal(12, (Value.new(3) * Value.new(4)).data)
+    assert_equal(81, (Value.new(3)**4).data)
+    assert_equal(-3, -Value.new(3).data)
+    assert_equal(-1, (Value.new(3) - Value.new(4)).data)
+    assert_equal(0.75, (Value.new(3) / Value.new(4)).data)
+    assert_in_delta(0.00005, 0.761594, Value.new(1).tanh.data)
+  end
 end
