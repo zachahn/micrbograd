d1c9389e106c5537c27d9a9324600faa18a94e4e
Zach Ahn <engineering@zachahn.com>
2024-01-11 19:16:13 -0700

    Implement the neural network

--- lib/micrbograd.rb
+++ lib/micrbograd.rb
@@ -2,15 +2,24 @@
 
 module Micrbograd
   class Value
+    def self.random
+      new(Kernel.rand * [1, -1].sample)
+    end
+
+    def self.with(*args)
+      args.map { |arg| new(arg) }
+    end
+
     def initialize(data, children: [], operation: nil)
       @data = data
       @children = children
       @operation = operation
-      @grad = 0
+      @gradient = 0.0
     end
 
-    attr_reader :data, :children, :operation, :grad
-    private :children, :operation
+    attr_accessor :data
+    attr_accessor :gradient
+    attr_reader :children, :operation
 
     def +(other)
       Value.new(data + other.data, children: [self, other], operation: :+)
@@ -22,7 +31,7 @@ module Micrbograd
 
     def **(other)
       raise TypeError, "Value `#{data}' Only accepts type `Numeric'. Got #{other}" unless other.is_a?(Numeric)
-      Value.new(data**other, children: [self], operation: :**)
+      Value.new(data**other, children: [self], operation: [:**, other])
     end
 
     def tanh
@@ -42,8 +51,133 @@ module Micrbograd
       self * (other**-1)
     end
 
+    def zero_gradient
+      self.gradient = 0.0
+    end
+
+    protected def backpropagate_local
+      case operation
+      in :+
+        a, b = children
+        a.gradient += gradient
+        b.gradient += gradient
+      in :*
+        a, b = children
+        a.gradient += b.data * gradient
+        b.gradient += a.data * gradient
+      in [:**, num]
+        a, = children
+        a.gradient += num * (a.data**(num - 1)) * gradient
+      in :tanh
+        a, = children
+        a.gradient += (1 - (Math.tanh(a.data)**2)) * gradient
+      in nil
+      else
+        raise "Unhandled operation: `#{operation.inspect}`"
+      end
+      nil
+    end
+
+    def backpropagate
+      topological_ordering = []
+      seen = {}
+      tsort = lambda do |node|
+        if !seen[node]
+          seen[node] = true
+          node.children.each do |child|
+            tsort.call(child)
+          end
+          topological_ordering.push(node)
+        end
+      end
+      tsort.call(self)
+
+      self.gradient = 1.0
+      topological_ordering.reverse_each { _1.backpropagate_local }
+    end
+
     def to_s
       "#<Micrbograd::Value @data=#{data}>"
     end
   end
+
+  module Shared
+    def zero_gradients
+      parameters.map { |parameter| parameter.zero_gradient }
+    end
+  end
+
+  class Neuron
+    include Shared
+
+    def initialize(number_of_inputs)
+      @weights = number_of_inputs.times.map { Value.random }
+      @bias = Value.random
+    end
+
+    attr_accessor :weights, :bias
+
+    def call(xs)
+      raise "Mismatching sizes #{@weights.size}, #{xs.size}" unless @weights.size == xs.size
+      actuality = @weights.zip(xs).map { |w, x| w * x }.reduce(:+) + @bias
+      actuality.tanh
+    end
+
+    def parameters
+      weights + [bias]
+    end
+  end
+
+  class Layer
+    include Shared
+
+    def initialize(number_of_inputs, number_of_neurons)
+      @neurons = number_of_neurons.times.map { Neuron.new(number_of_inputs) }
+    end
+
+    attr_accessor :neurons
+
+    def call(xs)
+      result = neurons.map { |neuron| neuron.call(xs) }
+      if result.size == 1
+        result.first
+      else
+        result
+      end
+    end
+
+    def parameters
+      neurons.flat_map do |neuron|
+        neuron.parameters
+      end
+    end
+  end
+
+  class MultiLayerPerceptron
+    include Shared
+
+    def initialize(number_of_inputs, list_of_number_of_neurons)
+      sz = [number_of_inputs] + list_of_number_of_neurons
+      @layers = sz.each_cons(2).map do |a, b|
+        Layer.new(a, b)
+      end
+    end
+
+    attr_accessor :layers
+
+    def call(xs)
+      layers.each do |layer|
+        xs = layer.call(xs)
+      end
+      xs
+    end
+
+    def parameters
+      layers.flat_map do |layer|
+        layer.parameters
+      end
+    end
+  end
+
+  MLP = MultiLayerPerceptron
 end
--- test/test_micrbograd.rb
+++ test/test_micrbograd.rb
@@ -5,13 +5,58 @@ require "test_helper"
 class TestMicrbograd < Minitest::Test
   include Micrbograd
 
-  def test_methods
+  def test_operations
     assert_equal(7, (Value.new(3) + Value.new(4)).data)
     assert_equal(12, (Value.new(3) * Value.new(4)).data)
     assert_equal(81, (Value.new(3)**4).data)
     assert_equal(-3, -Value.new(3).data)
     assert_equal(-1, (Value.new(3) - Value.new(4)).data)
     assert_equal(0.75, (Value.new(3) / Value.new(4)).data)
-    assert_in_delta(0.00005, 0.761594, Value.new(1).tanh.data)
+    assert_in_delta(0.00001, 0.761594, Value.new(1).tanh.data)
+
+    a = Value.new(3)
+    b = Value.new(1)
+    c = Value.new(4)
+    d = Value.new(1)
+    e = Value.new(5)
+    f = ((a + b) * (c - d)) / e
+    assert_in_delta(2.4, f.data, 0.00001)
+  end
+
+  def test_backpropagate
+    x1 = Value.new(2.0)
+    x2 = Value.new(0.0)
+    w1 = Value.new(-3.0)
+    w2 = Value.new(1.0)
+    b = Value.new(6.8813735870195432)
+    x1w1 = x1 * w1
+    x2w2 = x2 * w2
+    x1w1x2w2 = x1w1 + x2w2
+    n = x1w1x2w2 + b
+    o = n.tanh
+    o.backpropagate
+
+    assert_in_delta(1.0, o.gradient, 0.0000001)
+    assert_in_delta(0.5, n.gradient, 0.0000001)
+    assert_in_delta(0.5, b.gradient, 0.0000001)
+    assert_in_delta(0.5, x1w1x2w2.gradient, 0.0000001)
+    assert_in_delta(0.5, x2w2.gradient, 0.0000001)
+    assert_in_delta(0.5, x1w1.gradient, 0.0000001)
+    assert_in_delta(0.0, w2.gradient, 0.0000001)
+    assert_in_delta(0.5, x2.gradient, 0.0000001)
+    assert_in_delta(1.0, w1.gradient, 0.0000001)
+    assert_in_delta(-1.5, x1.gradient, 0.0000001)
+  end
+
+  def test_backpropagate_repeated
+    a = Value.new(5.0)
+    b = a + a
+    b.backpropagate
+
+    assert_in_delta(10.0, b.data, 0.000001)
+    assert_in_delta(5.0, a.data, 0.000001)
+
+    assert_in_delta(1.0, b.gradient, 0.000001)
+    assert_in_delta(2.0, a.gradient, 0.000001)
   end
 end
